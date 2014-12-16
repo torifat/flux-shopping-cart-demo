18c3228b805a0f7cce4e49a8a7934c01ede90066
Rifat Nabi <to.rifat@gmail.com>
2014-12-16 20:39:54 +0600

    added total in cart

--- js/components/Cart.react.js
+++ js/components/Cart.react.js
@@ -26,11 +26,22 @@ var Cart= React.createClass({
     var products = this.state.products.map(function (product) {
       return <CartItem key={product.id} value={product} />
     });
+    var total = this.state.products.reduce(function (acc, product) {
+      return acc + product.subTotal;
+    }, 0.0);
     return (
       <div>
         <h2>Cart</h2>
         <table>
-          {products}
+          <tbody>
+            {products}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan="3" className="text-right">Total</td>
+              <td className="text-right">${total}</td>
+            </tr>
+          </tfoot>
         </table>
       </div>
     )
